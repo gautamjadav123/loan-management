4ad653d48615701f30938503492fb570afaa7a98
Gautam Jadav <gj23059@gmail.com>
2024-03-13 17:09:14 +0530

    new changes 2

--- client/src/pages/admin/AdminHome.js
+++ client/src/pages/admin/AdminHome.js
@@ -6,6 +6,7 @@ export default function AdminHome() {
       <WrapItem display={"flex"} alignItems={"center"} justifyContent={"right"}>
         <Button colorScheme="telegram">Add New Loans</Button>
       </WrapItem>
+      {/* ?mekke */}
     </>
   );
 }
